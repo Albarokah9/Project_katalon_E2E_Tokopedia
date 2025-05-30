d7ad5d8388506673ec6c6939e075d844a5164c6c
Albar <albareizy942@gmail.com>
2025-05-30 14:17:22 +0700

    push Project_katalon_E2E_Tokopedia

---
+++ Tokopedia/Object Repository/Register/afterDaftar/span_Ya, Benar.rs
@@ -0,0 +1,104 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>span_Ya, Benar</name>
+   <tag></tag>
+   <elementGuidId>82c3222c-e83f-46c5-84d5-554561ac8c9d</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//span[(text() = 'Ya, Benar' or . = 'Ya, Benar')]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[(text() = 'Ya, Benar' or . = 'Ya, Benar')]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>button.css-vhz208 > span.css-bp4m37</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>span</value>
+      <webElementGuid>8d72e7cd-6599-49f8-974d-5b561f784e1e</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>css-bp4m37</value>
+      <webElementGuid>00a60f46-9316-4308-a7cf-da5dfedc13a3</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Ya, Benar</value>
+      <webElementGuid>32891388-7dd4-41aa-9dd0-94dda94f9c89</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/div[@class=&quot;css-1umf4fo&quot;]/div[@class=&quot;css-1ar5wm2&quot;]/div[@class=&quot;css-19npxxt&quot;]/div[@class=&quot;css-s351g5&quot;]/button[@class=&quot;css-vhz208&quot;]/span[@class=&quot;css-bp4m37&quot;]</value>
+      <webElementGuid>fc8bae8c-5b29-405e-bda5-0c1a0c8aa8dd</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Pastikan E-mail yang kamu isi sudah benar untuk diverifikasi.'])[1]/following::span[1]</value>
+      <webElementGuid>c778ef01-5def-4982-bdd5-5b5765bc021f</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='projecttraveloka@gmail.com'])[1]/following::span[1]</value>
+      <webElementGuid>bccbb7bc-858b-45ba-b69e-8e2840ddb67a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Ubah'])[1]/preceding::span[1]</value>
+      <webElementGuid>b6f0442d-92b0-48c1-92c2-5a3bb21e097a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Ya, Benar']/parent::*</value>
+      <webElementGuid>1d02c0b0-6753-4a41-bb9f-e03e52f0dff3</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[3]/div/div/div[2]/button/span</value>
+      <webElementGuid>32e64361-86f2-497f-999a-38d20b8354f1</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//span[(text() = 'Ya, Benar' or . = 'Ya, Benar')]</value>
+      <webElementGuid>8b661312-1138-4156-87a2-7df1ad674ba9</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
